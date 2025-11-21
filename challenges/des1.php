681e073eeda2a229d14c6fdba7321c289db9f632
Lucas Salvático <l.salvatiko@gmail.com>
2025-11-20 23:54:16 -0300

    Add files via upload

---
+++ challenges/des1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Desafio 1</title>
+</head>
+<body>
+    <section>
+    <form action="backDes1.php" method="get">
+        <label for="num">Numero</label>
+        <input type="text" name="num">
+        <input type="submit" value="Enviar">
+    </form>        
+    </section>
+</html>
